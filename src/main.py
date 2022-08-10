f6af439460ea9ec840e40c137eaf5155678c70cf
Ondrejtra <koala.sopel@gmail.com>
2022-08-10 22:40:17 +0200

    Index rendering functional

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from flask import *
 app = Flask('KatSit', template_folder='templates', static_folder='static')
 
 @app.route('/')
-def hello_world():
-  return render_template('base.html')
+def index():
+  return render_template('index.html')
 
 app.run(host='0.0.0.0', port=8080)
