0824aa0565acf5df95cd21642a4b0fda9b568498
Ondrejtra <koala.sopel@gmail.com>
2022-08-11 15:19:26 +0200

    Final prototype version

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ import os
 import json as jsonfiles
 
 app = Flask('KatSit', template_folder='templates', static_folder='static')
+session_filename = os.path.join(app.static_folder, 'data', 'session.json')
+userdata_filename = os.path.join(app.static_folder, 'data', 'userdata.json')
 
 @app.route('/')
 def index():
@@ -10,21 +12,36 @@ def index():
 
 @app.route('/login')
 def login():
-  return render_template("login.html")
-
+  with open(session_filename) as f:
+    result = jsonfiles.load(f)
+    if 'username' in result and 'password' in result:
+        return redirect(url_for('dashboard'))
+    return render_template("login.html")
+    
 @app.route('/dashboard', methods = ['POST', 'GET'])
 def dashboard():
-    if request.method == 'GET':
-      filename = os.path.join(app.static_folder, 'data', 'session.json')
-      with open(filename) as f:
-        jsonfiles.load(result, f)
-        return render_template('dashboard.html',form_data = result)
-    
-    if request.method == 'POST':
-      result = request.form.to_dict(flat=False)
-      filename = os.path.join(app.static_folder, 'data', 'session.json')
-      with open(filename, 'w') as f:
-        jsonfiles.dump(result, f)
-      return render_template('dashboard.html',form_data = result)
+  global session_filename
+  global userdata_filename
+  with open(userdata_filename) as f:
+    user_data = jsonfiles.load(f)
+  if request.method == 'GET':
+    with open(session_filename) as f:
+      result = jsonfiles.load(f)
+    if 'username' in result and 'password' in result:
+      return render_template('dashboard.html',form_data = result, user_data = user_data)
+  
+  elif request.method == 'POST':
+    result = request.form.to_dict(flat=False)
+    with open(session_filename, 'w') as f:
+      jsonfiles.dump(result, f)
+    return render_template('dashboard.html',form_data = result, user_data = user_data)
+  
+@app.route('/logout')
+def logoout():
+  global session_filename
+  with open(session_filename, 'w') as f:
+    jsonfiles.dump({}, f)
+  return redirect(url_for('index'))
+
   
 app.run(host='0.0.0.0', port=8080)
