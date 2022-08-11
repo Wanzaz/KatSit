00886ac68f29cb3fcf7acf6aa4d45cfa1255ba28
Ondrejtra <koala.sopel@gmail.com>
2022-08-11 13:43:07 +0200

    Index additions, basic login and dashboard test
    Dashboard still needs its key functionality implemented
    Login works by storing data into json files

--- src/main.py
+++ src/main.py
@@ -1,8 +1,30 @@
 from flask import *
+import os
+import json as jsonfiles
+
 app = Flask('KatSit', template_folder='templates', static_folder='static')
 
 @app.route('/')
 def index():
   return render_template('index.html')
 
+@app.route('/login')
+def login():
+  return render_template("login.html")
+
+@app.route('/dashboard', methods = ['POST', 'GET'])
+def dashboard():
+    if request.method == 'GET':
+      filename = os.path.join(app.static_folder, 'data', 'session.json')
+      with open(filename) as f:
+        jsonfiles.load(result, f)
+        return render_template('dashboard.html',form_data = result)
+    
+    if request.method == 'POST':
+      result = request.form.to_dict(flat=False)
+      filename = os.path.join(app.static_folder, 'data', 'session.json')
+      with open(filename, 'w') as f:
+        jsonfiles.dump(result, f)
+      return render_template('dashboard.html',form_data = result)
+  
 app.run(host='0.0.0.0', port=8080)
